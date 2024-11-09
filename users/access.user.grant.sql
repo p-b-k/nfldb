595c1d6eff90365a72b0cfe37fa65f1595e1cf41
Paul B. Krause <paul.b.krause@gmail.com>
2024-11-09 09:43:33 -0500

    Updating permissions for access user

--- users/access.user.grant.sql
+++ users/access.user.grant.sql
@@ -1,4 +1,4 @@
 -- Grant the required acces on a database (nfl) to the access user;
 
-grant select, show view on nfl.* to 'access'@'localhost';
+grant select, show view, lock tables on nfl.* to 'access'@'localhost';
 
