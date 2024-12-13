f7eb453712c96c82578a047b34738b12cb6cec93
Paul B. Krause <paul.b.krause@gmail.com>
2024-12-13 16:03:34 -0500

    Starting to develop game.data()

--- funcs.r
+++ funcs.r
@@ -10,11 +10,24 @@ defc <- c( rgb( 0.80, 0.30, 0.20, 0.80)
 
 plot.div.pts <- function(d, colors = defc) boxplot(split(d$game_for - d$game_against, d$team), col=colors)
 
-# by(NFL.record[, c("won_game")], NFL.record$team, sum) / by(NFL.record[, c("won_game", "lost_game")], NFL.record$team, sum)
-
 through.week <- function (week) {
   wins <- by(NFL.record[1:week, c("won_game")], NFL.record$team, sum)
   games <- by(NFL.record[1:week, c("won_game", "lost_game")], NFL.record$team, sum)
   wins / games
 }
+
+game.data <- function (team, week) {
+  G <- team[team$week == week, ]
+  attach(G)
+  Frame <- data.frame( q.for     = c( q1_for, q2_for )
+                     , q.against = c( q1_against, q2_against )
+                     , c.diff    = c( q1_for - q1_against
+                                    , (q1_for + q2_for) - (q1_against + q2_against)
+                                    )
+                     )
+  detach()
+  Frame
+}
+
+reset.width <- function () {options("width" = as.numeric(Sys.getenv ("COLUMNS")))}
 
---
+++ views/CurrentWeek.view.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE VIEW CurrentWeek
+AS
+SELECT team                    as guest
+     , game_for                as away_score
+     , if (won_game, '*', ' ') as away_won
+     , if (won_game, ' ', '*') as home_won
+     , game_against            as home_score
+     , oponent                 as host
+FROM Record
+WHERE week = (SELECT int_val FROM settings WHERE name = 'current_week')
+AND   at_home = 'N'
+ORDER BY team
+;
