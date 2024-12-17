1e1249fc33ff990bb2a186963a5c98f89e90f6b2
Paul B. Krause <paul.b.krause@gmail.com>
2024-12-17 18:58:06 -0500

    Updating scores for week 15

--- graphs.r
+++ graphs.r
@@ -78,38 +78,116 @@ pbk.demo.bar.stacked <- function () {
   specie <- c(rep("sorgho" , 3) , rep("poacee" , 3) , rep("banana" , 3) , rep("triticum" , 3) )
   # so for our purposes: c(rep("Q1", 2), rep("Q2", 2), rep("Q3", 2), rep("Q4", 2))
   # Or, if there's overtime,  c(rep("Q1", 2), rep("Q2", 2), rep("Q3", 2), rep("Q4", 2), rep("OT", 2))
-  quarters <- c(rep("Q1" , 2) , rep("Q2" , 2) , rep("Q3" , 2) , rep("Q4" , 2) )
 
   # @ elemn defines the name of one of the stacks
   condition <- rep(c("normal" , "stress" , "Nitrogen") , 4)
   # for our purposes: rep(c("Eagles", "Steelers"), 4) or, if there's overtime, rep(c("Eagles", "Steelers"), 5)
-  teams <- rep(c("Eagles" , "Panthers") , 4)
   
   # 3 x 4, or 12, random values between 0 and 15, to represent the data to be graphed
   value <- abs(rnorm(12 , 0 , 15))
   # for our purposes it will be the scores, somehow arranged to match with the values
-  scores <- c(0, -3, 14, -7, 0, -6, 8, 0)
 
   # Create a data frame with the 3, 12 element arrays
-  demo.data <- data.frame(specie,condition,value)
+  data <- data.frame(specie,condition,value)
   # for our purposes we'll do much the same
-  game.data <- data.frame(quarters, teams, scores)
    
   # Stacked
-  # ggplot(game.data, aes( fill=condition
-  #                      , y=value
-  #                      , x=specie
-  #                      )
-  #       ) + geom_bar(position="stack", stat="identity")
-
-  ggplot(game.data, aes( fill=teams
-                       , y=scores
-                       , x=quarters
-                       )
-        ) + geom_bar(position="stack", stat="identity")
+  ggplot(data, aes( fill=condition, y=value, x=specie))      +
+  geom_bar(position="stack", stat="identity")
 }
 
-# show.game.data <- function (data) {
-#   
-# }
+# Now try a mockup of an Eagles game (week 14 vs Carolina)
+pbk.demo.bar.stacked.eagles.14 <- function () {
+  # create a dataset
+  
+  quarters <- c(rep("Q1" , 2) , rep("Q2" , 2) , rep("Q3" , 2) , rep("Q4" , 2) )
+  teams <- rep(c("Eagles" , "Panthers") , 4)
+  scores <- c(0, -3, 14, -7, 0, -6, 8, 0)
+
+  data <- data.frame(quarters, teams, scores)
+  ggplot(data, aes( fill=teams, y=scores, x=quarters))       +
+  geom_bar(position="stack", stat="identity")
+}
+
+
+## *********************************************************************************************************************
+## Multiple small charts
+## See: https://r-graph-gallery.com/48-grouped-barplot-with-ggplot2.html
+## *********************************************************************************************************************
+
+# This is for the colors, it appears
+library(viridis)
+
+pbk.demo.bar.multi <- function () {
+  # The four sub graphs, with three values each
+  specie <- c(rep("sorgho" , 3) , rep("poacee" , 3) , rep("banana" , 3) , rep("triticum" , 3) )
+  
+  # The three values that are mapped on each graph
+  condition <- rep(c("normal" , "stress" , "Nitrogen") , 4)
+
+  # Random values (12, 3 for each of the 4 graphs)
+  value <- abs(rnorm(12 , 0 , 15))
+
+  # Make the data as before
+  data <- data.frame(specie,condition,value)
+   
+  # Graph
+  
+  # This appears to create a graph object
+  ggplot(data, aes(fill=condition, y=value, x=condition))    + 
+  # It looks like this is what makes the actual graphs
+  geom_bar(position="dodge", stat="identity")                +
+  # Some kind of formatting, perhaps?
+  scale_fill_viridis(discrete = T, option = "E")             +
+  # Clearly adding the overall title
+  ggtitle("Studying 4 species..")                            +
+  # This must be saying on what value to create the separate graphs
+  facet_wrap(~specie)                                        +
+  # This I presume is more formatting, I guess colors?
+  theme_ipsum()                                              +
+  # Presumably turning off the legend, for space I suppose
+  theme(legend.position="none")                              +
+  # Turning off x labels?
+  xlab("")                                                   
+}
+
+pbk.demo.bar.multi.eagles.pre.bye <- function () {
+  # The four sub graphs, with three values each
+  specie <- c(rep("Eagles" , 3) , rep("Opponent" , 3))
+  
+  # The three values that are mapped on each graph
+  condition <- rep(c("normal" , "stress" , "Nitrogen") , 4)
+
+  # Random values (12, 3 for each of the 4 graphs)
+  # value <- abs(rnorm(12 , 0 , 15))
+  value <- 1:12
+
+  # Make the data as before
+  data <- data.frame(specie,condition,value)
+   
+  # Graph
+  
+  # This appears to create a graph object
+  # Changing fill from condition to specie changes the color to be the same for all the bars on the subgraph but
+  # different from one sub-graph to the other, instead off the bars within a graph being heterochromatic, but the same
+  # colors in all the graphs
+  ggplot(data, aes(fill=condition, y=value, x=condition))    + 
+  # It looks like this is what makes the actual graphs
+  # Puts bars on the charts for the data
+  geom_bar(position="stack", stat="identity")                +
+  # Sets the color scheme
+  scale_fill_viridis(discrete = T, option = "E")             +
+  # Clearly adding the overall title
+  ggtitle("Studying 4 species..")                            +
+  # This must be saying on what value to create the separate graphs
+  facet_wrap(~specie)                                        +
+  # Formats the individual graph backgrounds (also scales and such)
+  theme_ipsum()                                              +
+  # Presumably turning off the legend, for space I suppose
+  theme(legend.position="none")                              +
+  # Turning off x labels?
+  xlab("")                                                   
+}
+
+
 
