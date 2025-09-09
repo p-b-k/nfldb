3c94d8d185c55a0f3e7dde6e300a106bbc6b00ec
Paul B. Krause <paul.b.krause@gmail.com>
2025-09-09 16:27:23 -0400

    Adding more files that got missed

---
+++ bad-cat/nfldb.scm
@@ -0,0 +1,22 @@
+(define-module (bad-cat nfldb)
+
+  ;; *******************************************************************************************************************
+  ;; Module to evaluate serialization expressions in
+  ;; *******************************************************************************************************************
+
+  #:use-module (oop goops)
+
+  #:use-module (bad-cat nfldb cache)
+
+  #:use-module (bad-cat nfldb cache league)
+  #:use-module (bad-cat nfldb cache schedule)
+
+  #:use-module (bad-cat nfldb team)
+  #:use-module (bad-cat nfldb game)
+  #:use-module (bad-cat utils)
+
+  #:export (nfldb-eval)
+)
+
+(define (nfldb-eval expr) (eval expr (resolve-module '(bad-cat nfldb))))
+
---
+++ bad-cat/nfldb/cache/game.scm
@@ -0,0 +1,24 @@
+(define-module (bad-cat nfldb cache game)
+
+  ;; *******************************************************************************************************************
+  ;; Cache for Schedule data
+  ;; Very static. Might change if a game gets flexed
+  ;; *******************************************************************************************************************
+
+  #:use-module (oop goops)
+
+  #:use-module (bad-cat utils)
+
+  #:use-module (bad-cat nfldb cache)
+  #:use-module (bad-cat nfldb espn)
+  #:use-module (bad-cat nfldb team)
+  #:use-module (bad-cat nfldb json)
+  #:use-module (bad-cat nfldb game)
+)
+
+(define-class <game-cache> (<nfldb-cache>)
+  (game       #:init-form     #f
+              #:getter        game.game
+              #:setter        game.game!)
+)
+
