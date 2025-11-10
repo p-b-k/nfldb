a6f461f091b6020205bd8370ea8864e1d0885ddc
Paul B. Krause <paul.b.krause@gmail.com>
2025-11-10 14:07:37 -0500

    Updating, and making schedule bar better

--- bad-cat/nfldb/ui/panels/schedulebar.scm
+++ bad-cat/nfldb/ui/panels/schedulebar.scm
@@ -33,6 +33,7 @@
     (if (eq? (game.home game) (team.nick team))
       (game.away game)
       (game.home game)))
+  (define (get-team) (team.nick team))
   (define (get-resource)
     (if game
       (let ( (result (game.result game)) )
@@ -45,19 +46,33 @@
           "not-played"))
       "bye"))
   (let ( (res (format #f "/bad-cat/nfldb/~a" (get-resource)))
-         (opp (if game (format #f "/bad-cat/nfldb/~a/logo" (get-opponent)) #f)) )
+         (opp-res (if game (format #f "/bad-cat/nfldb/~a/logo" (get-opponent)) #f))
+         (team-res (if game (format #f "/bad-cat/nfldb/~a/logo" (get-team)) #f)) )
     (let ( (vbox  (make-instance <gtk-box> #:orientation 'vertical #:homogeneous #t))
            (status-image (gtk-image-new-from-resource res))
-           (opponent-image (if opp (gtk-image-new-from-resource opp) (make-instance <gtk-image>))) )
+           (opponent-image (if opp-res (gtk-image-new-from-resource opp-res) (make-instance <gtk-image>)))
+           (team-image (if team-res (gtk-image-new-from-resource team-res) (make-instance <gtk-image>))) )
       (slot-set! status-image 'height-request 32)
       (slot-set! status-image 'width-request  32)
       (slot-set! status-image 'css-classes '("schedule-record-entry"))
       (slot-set! opponent-image 'height-request 32)
       (slot-set! opponent-image 'width-request  32)
       (slot-set! opponent-image 'css-classes '("schedule-record-entry"))
+      (slot-set! team-image 'height-request 32)
+      (slot-set! team-image 'width-request  32)
+      (slot-set! team-image 'css-classes '("schedule-record-entry"))
 
-      (gtk-box-append vbox opponent-image)
-      (gtk-box-append vbox status-image)
+      (if game
+        (if (eq? (game.home game) (team.nick team))
+          (begin
+            (gtk-box-append vbox opponent-image)
+            (gtk-box-append vbox status-image)
+            (gtk-box-append vbox team-image))
+          (begin
+            (gtk-box-append vbox team-image)
+            (gtk-box-append vbox status-image)
+            (gtk-box-append vbox opponent-image)))
+        (gtk-box-append vbox status-image))
       
       vbox)))
 
